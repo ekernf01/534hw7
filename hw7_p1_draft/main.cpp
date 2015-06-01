dbaf01d651204602e441d8dd825ec9c8de423e33
Eric Kernfeld <eric.kern13@gmail.com>
2015-05-31 19:54:19 -0700

    work up to first compile attempt

--- hw7_p1_draft/main.cpp
+++ hw7_p1_draft/main.cpp
@@ -3,6 +3,10 @@
 
  This program draws 10,000 multivariate Normal variates with (theoretical)
  covariance matching the sample covariance from erdata.txt.
+ It is intended to be readable rather than maximally efficient.
+ One stumbling block: GSL's column and row-getting functions return GSL vectors,
+ but GSL's covariance function demands simple arrays, as in double*.
+ That mismatch prompted much of the bad design that went into this program.
  
 */
 
@@ -17,46 +21,116 @@ int main()
 {
   int n = 158; //sample size
   int p = 51; //number of variables
-  int num_preds = p-1;//number of predictors
   char datafilename[] = "erdata.txt"; //name of the data file
   char outputfilename[] = "eric_cov_10_000.txt";
 
-  //allocate the data matrix
-  double** data = allocmatrix(n,p);
-
   //read the data
-  readmatrix(datafilename,n,p,data);
-  
-  //Discard the first column, which contains the response
-  int* row_inds = new int[n];
-  int* col_inds = new int[num_preds];
-  for(int i=1; i++; i<=n){
-    row_inds[i] = i;
-  }
-  for(int i=1; i++; i<=(num_preds)){
-    col_inds[i] = i;
-  }
-  gsl_matrix* predictors_only = MakeSubmatrix(data,n, row_inds, num_preds, col_inds);
+  double** data = readmatrix(datafilename,n,p,data);
 
+  //Get the covariance
+  gsl_matrix* cov_or_sqrt_cov = eric_covariance(data, p, n);
   
-  
-  //Obtain the covariance matrix
-  gsl_matrix* cov_or_sqrt_cov = gsl_matrix_alloc(num_preds,num_preds)
-  for(int i=1; i++; i<=num_preds){
-    for(int j=1; j++; j<=num_preds){
-      if(i<=j){
-        cov_entry = gsl_stats_covariance(col_array[i], 1, col_array[j], 1, n); //The extra ones say not to skip elements.
-        gsl_matrix_set(cov_or_sqrt_cov, i-1, j-1, cov_entry);//GSL indexes from 0
-        gsl_matrix_set(cov_or_sqrt_cov, j-1, i-1, cov_entry);
+  //Do Cholesky. GSL leaves garbage above the diagonal. The loops below remove it.
+  gsl_linalg_cholesky_decomp(cov_or_sqrt_cov);
+  for(int i=0; i<p; i++){
+    for(int j=0; j<p; j++){
+      if(i<j){
+        gsl_matrix_set(cov_or_sqrt_cov, i, j, 0);
       }
     }
   }
 
+  //rng setup
+  const gsl_rng_type * my_gsl_rng_type;
+  gsl_rng * my_gsl_rng;
+  gsl_rng_env_setup();
+  my_gsl_rng_type = gsl_rng_default;
+  my_gsl_rng = gsl_rng_alloc(T);
+  
+  //Generate the actual random numbers
+  int samp_size = 10000;
+  gsl_matrix* sample_isotropic = gsl_matrix_alloc(samp_size, p)
+  for(int i=0; i<samp_size; i++){
+    for(int j=0; j<p; j++){
+      gsl_matrix_set(sample_isotropic, i, j, gsl_ran_ugaussian(my_gsl_rng))
+    }
+  }
+  
+  //Transform them to have the right covariance
+  //copy them, transposed, into a crude array
+  gsl_matrix* sample_correct = gsl_matrix_alloc(samp_size, p)
+  matrixproduct(cov_or_sqrt_cov, sample_isotropic, sample_correct);
+  double** sample_correct_array = new double*[p]
+  for(int i=0; i<p; i++){
+    sample_correct_array[i] = new double[samp_size];
+    for(int j=0; j<samp_size; j++){
+      sample_correct_array[i][j] = gsl_matrix_get(sample_correct, j,i);
+    }
+  }
+  
+  //get the sample covariance for the synthetic data
+  gsl_matrix* synth_samp_cov = eric_covariance(sample_correct_array, p, n);
+
+  //print it
+  string format = "%f"
+  FILE* out = fopen(outputfilename,"w");
+  if(NULL==out){
+    printf("Cannot open output file [%s]\n",filename);
+    exit(1);
+  }
+  int gsl_matrix_fprintf (out, synth_samp_cov, format)
+  fclose(out);
+  
   //free memory
-  freematrix(n,data);
+  freematrix(p,data);
+  gsl_matrix_free(synth_samp_cov);
+  freematrix(p,sample_correct_array);
+  gsl_matrix_free(sample_isotropic);
+  gsl_matrix_free(sample_correct);
   gsl_matrix_free(predictors_only);
   delete col_inds;
   delete row_inds;
   
   return(1);
+}
+
+//reads from a file a matrix with n rows and p columns
+//allocates and returns the 2d array m so that m[p-1][n-1] is valid
+void readmatrix(char* filename,int n,int p)
+{
+  double** m = new double*[p]
+  int i,j;
+  double s;
+  FILE* in = fopen(filename,"r");
+  
+  if(NULL==in){
+    printf("Cannot open input file [%s]\n",filename);
+    exit(1);
+  }
+  for(j=0;j<p;j++){
+    m[j] = new double[n];
+    for(i=0;i<n;i++){
+      fscanf(in,"%lf",&s);
+      m[j][i] = s;
+    }
+  }
+  fclose(in);
+  return;
+}
+
+//This function returns a GSL matrix containing the covariance of the variables stored in data.
+//data[num_vars-1][num_cases-1] should return a double.
+//This function allocates memory.
+gsl_matrix* eric_covariance(double** data, int num_vars, int num_cases){
+  gsl_matrix* cov = gsl_matrix_alloc(p,p)
+  for(int i=0; i<num_vars; i++){
+    for(int j=0; j<num_vars; j++){
+      if(i<=j){
+        cov_entry = gsl_stats_covariance(data[i], 1, data[j], 1, num_cases); //The extra ones say not to skip elements.
+        gsl_matrix_set(cov, i, j, cov_entry);
+        gsl_matrix_set(cov, j, i, cov_entry);
+      }
+    }
+  }
+  return cov
 }
--- hw7_p1_draft/matrices.h
+++ hw7_p1_draft/matrices.h
@@ -3,28 +3,33 @@
 #include <string.h>
 #include <stdlib.h>
 
-extern "C" {
-#include <clapack.h>
-}
+//THESE ARE GSL FUNCTIONS
+//YOU DO NOT NEED TO INCLUDE ALL THESE HEADER FILES IN YOUR CODE
+//JUST THE ONES YOU ACTUALLY NEED;
+//IN THIS APPLICATION, WE ONLY NEED gsl/gsl_matrix.h
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_statistics.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_cdf.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_permutation.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_sort_double.h>
+#include <gsl/gsl_sort_vector.h>
+#include <gsl/gsl_errno.h>
 
-extern "C" {
- void dpotri_(char*,int*,double*,int*,int*);
- void dgeev_(char*, char*, int*, double*, int*, double*, double*, double*,
-	 int*, double*, int*, double*, int*, int*);
-}
+void printmatrix(char* filename,gsl_matrix* m);
+gsl_matrix* transposematrix(gsl_matrix* m);
+void matrixproduct(gsl_matrix* m1,gsl_matrix* m2,gsl_matrix* m);
+gsl_matrix* inverse(gsl_matrix* K);
+gsl_matrix* MakeSubmatrix(gsl_matrix* M,
+			  int* IndRow,int lenIndRow,
+			  int* IndColumn,int lenIndColumn);
+double logdet(gsl_matrix* K);
 
-//general functions
-double ** allocmatrix(int n,int p);
-void freematrix(int n,double** m);
-void copymatrix(int n,int p,double** source,double** dest);
-void readmatrix(char* filename,int n,int p,double* m[]);
-void printmatrix(char* filename,int n,int p,double** m);
-double** transposematrix(int n,int p,double** m);
-void dotmatrixproduct(int n,int p,double** m1,double** m2,double** m);
-void matrixproduct(int n,int p,int l,double** m1,double** m2,double** m);
-void inverse(int p,double** m);
-double logdet(int p,double** m);
+//Eric's additions
+double logmarglik(gsl_matrix* data,int lenA,int* A);
 
-//functions related to HW2
-double** submatrix(int n,int p,double** data,int lenA,int* A);
-double marglik(int n,int p,double** data,int lenA,int* A);
